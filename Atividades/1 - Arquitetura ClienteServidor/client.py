3d6f9c6fb4d759bed3f791b87ab3fb9f384fbe0b
Hugo Lima Romão <hugo8romao@gmail.com>
2023-05-11 07:49:55 -0400

    Atividade 1

--- Atividades/1 - Arquitetura ClienteServidor/client.py
+++ Atividades/1 - Arquitetura ClienteServidor/client.py
@@ -1,28 +1,17 @@
 import socket
 from cryptography.fernet import Fernet
 
-# Chave secreta compartilhada entre o cliente e o servidor
-# Certifique-se de usar a mesma chave secreta utilizada no servidor
-chave_secreta = b'chave_secreta_aqui'
-
 HOST = 'localhost'
-PORT = 5000
+PORT = 5058
+SECRET_KEY = b'ZmDfcTF7_60GrrY167zsiPd67pEvs0aGOv2oasOM1Pg='
 
-def enviar_mensagem(mensagem):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((HOST, PORT))
+fernet = Fernet(SECRET_KEY)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+mensagem_decodificada = 'Ola servidor'.encode()
+mensagem_codificada = fernet.encrypt(mensagem_decodificada)
 
-    # Criando um objeto Fernet com a chave secreta
-    fernet = Fernet(chave_secreta)
-
-    # Codificando a mensagem
-    mensagem_codificada = fernet.encrypt(mensagem.encode())
-
-    s.sendall(mensagem_codificada)
-
-    data = s.recv(1024)
-    print(f'Resposta do servidor: {data.decode()}')
-    s.close()
-
-mensagem = 'Olá, servidor!'
-enviar_mensagem(mensagem)
+s.sendall(mensagem_codificada)
+data = s.recv(1024)
+print(f'Resposta do servidor: {data.decode()}')
+s.close()
--- Atividades/1 - Arquitetura ClienteServidor/server.py
+++ Atividades/1 - Arquitetura ClienteServidor/server.py
@@ -2,22 +2,18 @@ import socket
 import threading
 from cryptography.fernet import Fernet
 
-# Chave secreta compartilhada entre o cliente e o servidor
-chave_secreta = b'chave_secreta_aqui'
-
 HOST = ''
-PORT = 7000
+PORT = 5058
+SECRET_KEY = b'ZmDfcTF7_60GrrY167zsiPd67pEvs0aGOv2oasOM1Pg='
 
-def handle_client(conn, addr):
+def handle_connection(conn, addr):
+    fernet = Fernet(SECRET_KEY)
+
     print(f'Conectado por {addr}')
-    
-    # Criando um objeto Fernet com a chave secreta
-    fernet = Fernet(chave_secreta)
-
     data = conn.recv(1024)
     mensagem_cifrada = data.decode()
-    mensagem_decifrada = fernet.decrypt(mensagem_cifrada.encode())
-    print(f'Mensagem recebida: {mensagem_decifrada.decode()}')
+    mensagem_decifrada = fernet.decrypt(mensagem_cifrada)
+    print(f'Mensagem recebida: {mensagem_decifrada}')
 
     conn.sendall('Mensagem recebida com sucesso!'.encode())
     conn.close()
@@ -25,13 +21,12 @@ def handle_client(conn, addr):
 def start_server():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((HOST, PORT))
-    s.listen(5)  # Aumentamos o número máximo de conexões simultâneas
-
+    s.listen(5)
     print(f'Servidor escutando na porta {PORT}...')
 
     while True:
         conn, addr = s.accept()
-        client_thread = threading.Thread(target=handle_client, args=(conn, addr))
-        client_thread.start()
+        thread = threading.Thread(target=handle_connection, args=(conn, addr))
+        thread.start()
 
 start_server()
